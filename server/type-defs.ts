4c3b5d49ce36efc10898b096e5f19f6ad2c6f90b
Safira Nugroho <safiranugroho@gmail.com>
2019-11-03 19:00:21 +1100

    set up gql, eslint, prettier, workspaces

---
+++ server/type-defs.ts
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+  type Event {
+    name: String;
+    date: String;
+    time: String;
+    venue: String;
+    link: String;
+  }
+
+  type Query {
+    events: [Event]
+  }
+`;
